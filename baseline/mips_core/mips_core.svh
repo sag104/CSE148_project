379f872b3b1bebc629cb426e39e71e2d7b690996
sag104 <38965946+sag104@users.noreply.github.com>
2023-06-04 19:32:02 -0700

    Delete baseline directory

--- baseline/mips_core/mips_core.svh
+++
@@ -1,4 +0,0 @@
-`define DATA_WIDTH 32
-`define ADDR_WIDTH 26
-
-import mips_core_pkg::*;
--- baseline/obj_dir/memory_driver.d
+++
@@ -1,8 +0,0 @@
-memory_driver.o: ../memory_driver.cpp ../memory_driver.h Vmips_core.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/verilated.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/verilatedos.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/verilated_config.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/verilated_types.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/verilated_funcs.h \
- /home/linux/ieng6/cs148sp23/public/tools/oss-cad-suite/share/verilator/include/vltstd/svdpi.h \
- ../memory.h
